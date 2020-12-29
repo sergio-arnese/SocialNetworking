ffa24bd926097e76ce95f1c617cfaf037fa2323b
sergio arnese <sergio.arnese@gmail.com>
2020-12-29 18:09:25 +0100

    Initial commit: added pom.xml and entry point for application

---
+++ src/main/java/it/sergio/arnese/kata/socialnetworking/Appl.java
@@ -0,0 +1,12 @@
+package it.sergio.arnese.kata.socialnetworking;
+
+public class Appl {
+
+    public static void main(String[] args) {
+        new Appl().go();
+    }
+
+    public void go() {
+
+    }
+}
